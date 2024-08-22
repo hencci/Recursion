ad4d0305ba006df416363a0eb1cdfa61538f826a
hencci <hencci20@gmail.com>
2024-08-22 10:05:35 +0100

    Split the array into 2 halves

--- mergeSort.js
+++ mergeSort.js
@@ -1,3 +1,7 @@
 function mergeSort(arr) {
     if (arr.length <= 1) return arr;
+
+    const median = Math.floor(arr.length / 2);
+    const leftHalf = mergeSort(arr.slice(0, median));
+    const rightHalf = mergeSort(arr.slice(median));
 }
