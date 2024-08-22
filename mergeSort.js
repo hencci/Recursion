aff54b5e491f60006b0101178f91b454c6313154
hencci <hencci20@gmail.com>
2024-08-22 10:41:20 +0100

    compare and merged both halves

--- mergeSort.js
+++ mergeSort.js
@@ -10,4 +10,16 @@ function merge(L, R) {
     const sortedArray = [];
     let LIndex = 0;
     let RIndex = 0;
+
+    while (LIndex < L.length && RIndex < R.length) {
+        if (L[LIndex] < R[RIndex]) {
+            sortedArray.push(L[LIndex]);
+            LIndex++;
+        } else {
+            sortedArray.push(R[RIndex]);
+            RIndex++;
+        }
+    }
+
+    return sortedArray.concat(L.slice(LIndex)).concat(R.slice(RIndex));
 }
